81c70c060bcf381210a3cd30a45101c85d60aa56
Marcos Reis <marcosreeismr@gmail.com>
2022-01-05 08:01:23 -0300

    Incluindo testes

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,38 @@
+import { ICreateUsersDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { UsersRepository } from "../implementations/UsersRepository";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create({
+    driver_license,
+    email,
+    name,
+    password,
+  }: ICreateUsersDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, {
+      driver_license,
+      email,
+      name,
+      password,
+    });
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find((user) => user.email === email);
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+}
+
+export { UsersRepositoryInMemory };
---
+++ src/modules/accounts/useCases/authenticateUser/AuthentucateUserUseCase.spec.ts
@@ -0,0 +1,64 @@
+import { AppError } from "../../../../erros/AppError";
+import { ICreateUsersDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory
+    );
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("should be able to authenticate a user", async () => {
+    const user: ICreateUsersDTO = {
+      driver_license: "000123",
+      email: "user@teste.com",
+      password: "123",
+      name: "User Teste",
+    };
+
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("should not be able to authenticate as nonexistent user", () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: "false@email.com",
+        password: "1234",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to authenticate with incorrect password", () => {
+    expect(async () => {
+      const user: ICreateUsersDTO = {
+        driver_license: "999",
+        email: "user@user.com",
+        password: "1234",
+        name: "User Teste Error",
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({
+        email: user.email,
+        password: "1",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
