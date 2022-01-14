1fd2340de6a50fe0c2f962c88e716419986e6514
Marcos Reis <marcosreeismr@gmail.com>
2022-01-14 09:15:03 -0300

    Adicionando useCase uoload de imagens do carro

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,9 +4,11 @@ import { UsersRepository } from "../../modules/accounts/infra/typeorm/repositori
 import { UsersTokensRepository } from "../../modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepositoty } from "../../modules/accounts/repositories/IUsersTokensRepository";
+import { CarsImagesRepository } from "../../modules/cars/infra/typeorm/repositories/CarsImagesRepository";
 import { CarsRepository } from "../../modules/cars/infra/typeorm/repositories/CarsRepository";
 import { CategoriesRepository } from "../../modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationRepository } from "../../modules/cars/infra/typeorm/repositories/SpecificationsRepository";
+import { ICarsImagesRepository } from "../../modules/cars/repositories/ICarsImagesRepository";
 import { ICarsRepository } from "../../modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
@@ -29,6 +31,11 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUsersTokensRepositoty>(
   "UsersTokensRepository",
   UsersTokensRepository
+);
+
+container.registerSingleton<ICarsImagesRepository>(
+  "CarsImagerepository",
+  CarsImagesRepository
 );
 
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
